ecf935762015f501cb4087202a32ed21c0410c99
GlennBarosen <glennbarosen@gmail.com>
2020-08-24 19:07:51 +0200

    Displays list of fruit elements 🍐

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { eventNames } from 'process';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type FruitProps = {
+  name: string,
+  taste: string,
+  texture: string
 }
+
+const App = () => {
+
+  const [fruit, setFruit] = useState({
+    name: '',
+    taste: '',
+    texture: ''
+  });
+  const [fruitList, setFruitList] = useState(Array());
+
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    setFruit({
+      ...fruit,
+      [e.target.name]: value
+    });
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setFruitList([
+      ...fruitList,
+      {
+        id: fruitList.length + 1,
+        name: fruit.name,
+        taste: fruit.taste,
+        texture: fruit.texture
+      }]);
+    setFruit({
+      name: '',
+      taste: '',
+      texture: ''
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>What is the name of the fruit?</label>
+        <input
+          name='name'
+          type='text'
+          value={fruit.name}
+          autoComplete='off'
+          onChange={handleChange} />
+
+        <label>How does it taste?</label>
+        <input
+          name='taste'
+          type='text'
+          value={fruit.taste}
+          autoComplete='off'
+          onChange={handleChange} />
+
+        <label>What is the texture like?</label>
+        <input
+          name='texture'
+          type='text'
+          value={fruit.texture}
+          autoComplete='off'
+          onChange={handleChange} />
+
+        <button>LOG THIS FRUIT</button>
+      </form>
+
+      <ul>
+        {fruitList.map((fruit) => (
+          <div key={fruit.id}>
+            <li>
+              Name: {fruit.name} <br />
+              Taste: {fruit.taste} <br />
+              Texture: {fruit.texture}
+            </li>
+            <br />
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
 
 export default App;
